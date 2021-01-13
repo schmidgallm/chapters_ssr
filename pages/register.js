b03bfa3186792d435fb464664f2ca15dda0c4fb1
Michael Schmidgall <schmidgallm@outlook.com>
2021-01-13 00:25:31 -0700

    remove gtag event from register page

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,6 @@
 // Dependencies
 import React, { useEffect } from 'react';
 
-// Gtag for analytics
-import * as gtag from '../lib/gtag';
-
 // Components
 import AuthWrapper from '../components/Layout/AuthWrapper';
 import RegisterHeader from '../components/Pages/Register/RegisterHeader';
@@ -13,13 +10,6 @@ import RegisterForm from '../components/Auth/RegisterForm';
 import styles from '../styles/register.module.css';
 
 export default function register() {
-  useEffect(() => {
-    gtag.event({
-      action: 'register page',
-      category: 'register',
-      label: 'pageview',
-    });
-  });
   return (
     <AuthWrapper>
       <div className={styles.register}>
